0810595eb7f54b614ade991e294f08d9e11964af
Nykram <nykram@wp.pl>
2021-07-17 18:13:40 +0200

    Added db config to .gitignore

--- app/common/smiteapiscripts.py
+++ app/common/smiteapiscripts.py
@@ -2,7 +2,7 @@ from json import load as json_load
 
 
 def read_config():
-    with open('../db/dbconfig.json', 'r') as dbconfig:
+    with open('../dbconfig.json', 'r') as dbconfig:
         config = json_load(dbconfig)
         server_name = config['database']['server_name']
         dbname = config['database']['dbname']
