ab11c3f95b7ba2dafea735d4ffdde01ae824c4fb
Mateusz Burdyna <mateuszburdyna@MacBook-Air-Mateusz.local>
2021-07-20 13:21:32 +0200

    cleaned up, added TOP lists

--- app/analyzer.py
+++ app/analyzer.py
@@ -1,4 +1,5 @@
 from smiteapi.smiteobjects import Match, Player, PlayerEntry
+import sys
 
 MAX_BATCH = 22
 
@@ -18,7 +19,6 @@ class Analyzer(object):
                     continue
                 self.items[item] = self.items.get(item, 0) + 1
         self.match_ids.append(match.id)
-        print("Match {} analyzed.".format(match.id))
 
     # since smite-api returns PlayerEntries(raw) instead of raw matches
     # we have to handle it separately
@@ -40,15 +40,17 @@ class Analyzer(object):
                 to_analyze[index].entries.append(PlayerEntry(match))
         for match in to_analyze:
             self.analyze(match)
+        return index + 1
 
     def analyze_queue(self, queue_id, date, hour=-1):
         response = self.api.make_request('getmatchidsbyqueue', [queue_id, date, hour])
         ids = list(map(lambda d: d['Match'], response))
+        total = len(ids)
         count = 0
         while ids != []:
             id_str = ','.join(ids[:MAX_BATCH])
             batch = self.api.make_request('getmatchdetailsbatch', [id_str])
             ids = ids[MAX_BATCH:]
-            self.analyze_batch(batch)
-            count += len(batch)
-        print('Queue {} with {} games analyzed.'.format(queue_id, count))
+            count += self.analyze_batch(batch)
+            print('\rAnalyzed {}/{} games.'.format(count, total), end='', flush=True)
+        print('\nQueue {} with {} games analyzed.'.format(queue_id, count))
--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from smiteapi.smite import Smite
+from smiteapi.smite import Smite, GODS_DICT, ITEMS_DICT
 from smiteapi.smiteobjects import Match
 from analyzer import Analyzer
 
@@ -10,6 +10,10 @@ DEV_ID = jsfile['devId']
 AUTH_KEY = jsfile['authKey']
 file.close()
 
+def sort_and_map(d1, d2):
+    mapped = {d2.get(k, ''): v for k, v in d1.items()}
+    return {k: v for k, v in sorted(mapped.items(), key=lambda d: d[1])}
+
 
 smite = Smite(DEV_ID, AUTH_KEY)
 
@@ -18,6 +22,10 @@ test = smite.get_player('LyQsPL')
 analyzer = Analyzer(smite)
 
 queue = test.latest_queue(smite)
-date = '20210717'
+date = smite.create_timestamp()[:8]
 
 analyzer.analyze_queue(queue, date, 3)
+
+gods_top = sort_and_map(analyzer.gods, GODS_DICT)
+items_top = sort_and_map(analyzer.items, ITEMS_DICT)
+
