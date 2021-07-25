4bcdfd3ae66e1b28aea7d10d624ab0843e27f446
Nykram <nykram@wp.pl>
2021-07-25 19:02:12 +0200

    Small adjustments

--- app/crons/motd.py
+++ app/crons/motd.py
@@ -1,8 +1,11 @@
-from app.main import smite
+from app.smiteapi.smite import Smite
+from app.smiteapi.smiteapiscripts import read_auth_config
 
 
 def run_save_motd():
     try:
+        keys = read_auth_config()
+        smite = Smite(keys[0], keys[1])
         smite.save_motd()
     except Exception as Error:
         return str(Error)
