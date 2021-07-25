35211df5429563e2da71183788b950113fb69f0b
Nykram <nykram@wp.pl>
2021-07-25 20:38:14 +0200

    Changed cron motd file.

--- app/crons/motd.py
+++ app/crons/motd.py
@@ -5,7 +5,7 @@ from smiteapi.smiteapiscripts import read_auth_config
 def run_save_motd():
     try:
         keys = read_auth_config()
-        smite = Smite(keys[0], keys[1])
+        smite = Smite()
         smite.save_motd()
     except Exception as Error:
         return str(Error)
