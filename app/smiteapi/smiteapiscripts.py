207b510674435e35e53b3cf567d8e7e594427e14
Nykram <nykram@wp.pl>
2021-07-25 13:36:58 +0200

    Changed readconfig function.

--- app/smiteapi/smiteapiscripts.py
+++ app/smiteapi/smiteapiscripts.py
@@ -1,9 +1,12 @@
 from json import load as json_load
+import sys, os
 
 
 def read_config():
     """read database config that is stored in json"""
-    with open('./dbconfig.json', 'r') as dbconfig:
+    project_dir = sys.path[1]
+    config_dir = os.path.join(project_dir, r"app\dbconfig.json")
+    with open(config_dir, 'r') as dbconfig:
         config = json_load(dbconfig)
         server_name = config['database']['server_name']
         dbname = config['database']['dbname']
