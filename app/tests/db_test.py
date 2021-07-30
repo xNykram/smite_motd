45368db3aaf758ca570d349ccf0b9a6a096170e5
Mateusz Burdyna <mateuszburdyna@macbook-air-mateusz.home>
2021-07-30 15:58:33 +0200

    deleted trash from unittest

--- app/tests/db_test.py
+++ app/tests/db_test.py
@@ -1,14 +1,7 @@
 import unittest
-import pymssql
 
 class DatabaseTest(unittest.TestCase):
     pass
 
 if __name__ == '__main__':
-    server = '51.68.140.249'
-    db = 'smiteAPI'
-    login = 'smiteAPI'
-    passwd = 'smiteapi684'
-    conn = pymssql.connect(server, login, passwd, db)
-    
     unittest.main()
