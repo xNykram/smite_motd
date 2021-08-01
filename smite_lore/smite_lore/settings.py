2cf0b5590a3cdc28148ada03f62fee64e3d55c47
Nykram <nykram@wp.pl>
2021-08-01 16:27:31 +0200

    Modify Django settings

--- smite_lore/smite_lore/settings.py
+++ smite_lore/smite_lore/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-2&_49v#j%8288fm-y(9fogao*lz)c7#=q-=y_2^xdcs21iaxs(
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['51.68.140.249', 'www.nykram.pl', 'http://nykram.pl']
+ALLOWED_HOSTS = ['51.68.140.249', 'www.nykram.pl', 'http://nykram.pl', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
